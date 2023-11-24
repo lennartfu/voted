bd382b5ceec723a2e1aefaeb6ef9235b372013e4
s84810 <s84810@bht-berlin.de>
2023-11-24 11:56:08 +0100

    Add placeholder template to views.py

--- product/views.py
+++ product/views.py
@@ -2,36 +2,54 @@ from django.shortcuts import render
 
 
 def home(request):
-    pass
+    return render(request, "product/base.html", {
+        "title": "Home",
+    })
 
 
 def login(request):
-    pass
+    return render(request, "product/base.html", {
+        "title": "Login",
+    })
 
 
 def register(request):
-    pass
+    return render(request, "product/base.html", {
+        "title": "Registrierung",
+    })
 
 
 def profile(request):
-    pass
+    return render(request, "product/base.html", {
+        "title": "Profil",
+    })
 
 
 def profile_edit(request):
-    pass
+    return render(request, "product/base.html", {
+        "title": "Profil bearbeiten",
+    })
 
 
 def settings(request):
-    pass
+    return render(request, "product/base.html", {
+        "title": "Einstellungen",
+    })
 
 
 def vote_create(request):
-    pass
+    return render(request, "product/base.html", {
+        "title": "Neue Abstimmung",
+    })
 
 
 def vote_code(request):
-    pass
+    return render(request, "product/base.html", {
+        "title": "Abstimmung",
+    })
 
 
 def log(request):
-    pass
+    return render(request, "product/base.html", {
+        "title": "Meine Abstimmungen",
+    })
