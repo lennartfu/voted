626b0972e3b27fc848b36d30b497850fe16d9034
s84810 <s84810@bht-berlin.de>
2023-11-24 13:40:56 +0100

    Restructure template files

--- product/views.py
+++ product/views.py
@@ -2,54 +2,54 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "product/base.html", {
+    return render(request, "base.html", {
         "title": "Home",
     })
 
 
 def login(request):
-    return render(request, "product/base.html", {
+    return render(request, "base.html", {
         "title": "Login",
     })
 
 
 def register(request):
-    return render(request, "product/base.html", {
+    return render(request, "base.html", {
         "title": "Registrierung",
     })
 
 
 def profile(request):
-    return render(request, "product/base.html", {
+    return render(request, "base.html", {
         "title": "Profil",
     })
 
 
 def profile_edit(request):
-    return render(request, "product/base.html", {
+    return render(request, "base.html", {
         "title": "Profil bearbeiten",
     })
 
 
 def settings(request):
-    return render(request, "product/base.html", {
+    return render(request, "base.html", {
         "title": "Einstellungen",
     })
 
 
 def vote_create(request):
-    return render(request, "product/base.html", {
+    return render(request, "base.html", {
         "title": "Neue Abstimmung",
     })
 
 
 def vote_code(request):
-    return render(request, "product/base.html", {
+    return render(request, "base.html", {
         "title": "Abstimmung",
     })
 
 
 def log(request):
-    return render(request, "product/base.html", {
+    return render(request, "base.html", {
         "title": "Meine Abstimmungen",
     })
